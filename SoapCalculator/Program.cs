420399e9f418ea1e59e92188239df7b76c3fb467
Harun Korkmaz <harun.korkmaz@fairteknoloji.com>
2025-08-19 16:44:19 +0300

    Refactor SOAP service to use DataContractSerializer and update request/response models. Introduced SunucuRequest and SunucuResponse records for improved data handling.

--- SoapCalculator/Program.cs
+++ SoapCalculator/Program.cs
@@ -1,4 +1,5 @@
 using System.ServiceModel;
+using System.Runtime.Serialization;
 using SoapCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,17 +9,31 @@ app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
-    _ = endpoints.UseSoapEndpoint<SunucuService>("/Servis.asmx", new SoapEncoderOptions(), SoapSerializer.XmlSerializer);
+    _ = endpoints.UseSoapEndpoint<SunucuService>("/Servis.asmx", new SoapEncoderOptions(), SoapSerializer.DataContractSerializer);
 });
 
+app.Urls.Add("http://localhost:5000");
 app.Run();
 
 [ServiceContract]
 public class SunucuService
 {
     [OperationContract]
-    public int Topla(int sayi1, int sayi2)
-    {
-        return sayi1 + sayi2;
-    }
+    SunucuResponse Topla(SunucuRequest request) => new SunucuResponse { Sonuc = request.Sayi1 + request.Sayi2 };
+}
+
+[DataContract]
+public record SunucuRequest
+{
+    [DataMember]
+    public int Sayi1 { get; set; }
+    [DataMember]
+    public int Sayi2 { get; set; }
+}
+
+[DataContract]
+public record SunucuResponse
+{
+    [DataMember]
+    public int Sonuc { get; set; }
 }
--- istemci/Program.cs
+++ istemci/Program.cs
@@ -1,17 +1,35 @@
 ﻿using System.ServiceModel;
 using System.ServiceModel.Channels;
+using System.Runtime.Serialization;
 
 var istemci = new Servis(new BasicHttpBinding(), new EndpointAddress("http://localhost:5000/Servis.asmx"));
-Console.WriteLine(istemci.Topla(1, 2));
+var sonuc = istemci.Topla(new SunucuRequest { Sayi1 = 1, Sayi2 = 2 });
+Console.WriteLine(sonuc.Sonuc);
 
 class Servis(Binding binding, EndpointAddress remoteAddress) : ClientBase<IServis>(binding, remoteAddress)
 {
-    public int Topla(int sayi1, int sayi2) => Channel.Topla(sayi1, sayi2);
+    public SunucuResponse Topla(SunucuRequest request) => Channel.Topla(request);
 }
 
 [ServiceContract]
 interface IServis
-{
+{       
     [OperationContract]
-    int Topla(int sayi1, int sayi2);
+    SunucuResponse Topla(SunucuRequest request);
+}
+
+[DataContract]
+public record SunucuRequest
+{
+    [DataMember]
+    public int Sayi1 { get; set; }
+    [DataMember]
+    public int Sayi2 { get; set; }
+}
+
+[DataContract]
+public record SunucuResponse
+{
+    [DataMember]
+    public int Sonuc { get; set; }
 }
