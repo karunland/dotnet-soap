2b9cfc75caeef15256deac943dee960e2378f601
Harun Korkmaz <harun.korkmaz@fairteknoloji.com>
2025-08-19 17:25:24 +0300

    s

--- SoapCalculator/Program.cs
+++ SoapCalculator/Program.cs
@@ -3,37 +3,59 @@ using System.Runtime.Serialization;
 using SoapCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<SunucuService>();
+
+// Service'i interface üzerinden ekliyoruz
+builder.Services.AddSingleton<ICalculatorService, CalculatorService>();
+
 var app = builder.Build();
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
-    _ = endpoints.UseSoapEndpoint<SunucuService>("/Servis.asmx", new SoapEncoderOptions(), SoapSerializer.DataContractSerializer);
+    endpoints.UseSoapEndpoint<ICalculatorService>("/Calculator.asmx",
+        new SoapEncoderOptions(),
+        SoapSerializer.DataContractSerializer);
 });
 
 app.Urls.Add("http://localhost:5000");
 app.Run();
 
+// ---- Service Contract ----
 [ServiceContract]
-public class SunucuService
+public interface ICalculatorService
 {
     [OperationContract]
-    SunucuResponse Topla(SunucuRequest request) => new SunucuResponse { Sonuc = request.Sayi1 + request.Sayi2 };
+    SunucuResponse Topla(SunucuRequest request);
+
+    [OperationContract]
+    SunucuResponse Cikar(SunucuRequest request);
 }
 
+// ---- Service Implementation ----
+public class CalculatorService : ICalculatorService
+{
+    public SunucuResponse Topla(SunucuRequest request) =>
+        new SunucuResponse { Sonuc = request.Sayi1 + request.Sayi2 };
+
+    public SunucuResponse Cikar(SunucuRequest request)
+    {
+        if (request.Sayi1 < request.Sayi2)
+            throw new FaultException("Sayi1, sayi2'den küçük olamaz.");
+
+        return new SunucuResponse { Sonuc = request.Sayi1 - request.Sayi2 };
+    }
+}
+
+// ---- Data Contracts ----
 [DataContract]
 public record SunucuRequest
 {
-    [DataMember]
-    public int Sayi1 { get; set; }
-    [DataMember]
-    public int Sayi2 { get; set; }
+    [DataMember] public int Sayi1 { get; set; }
+    [DataMember] public int Sayi2 { get; set; }
 }
 
 [DataContract]
 public record SunucuResponse
 {
-    [DataMember]
-    public int Sonuc { get; set; }
+    [DataMember] public int Sonuc { get; set; }
 }
--- istemci/Program.cs
+++ istemci/Program.cs
@@ -1,35 +1,49 @@
-﻿using System.ServiceModel;
+﻿using System;
+using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.Runtime.Serialization;
 
-var istemci = new Servis(new BasicHttpBinding(), new EndpointAddress("http://localhost:5000/Servis.asmx"));
+var binding = new BasicHttpBinding();
+var endpoint = new EndpointAddress("http://localhost:5000/Calculator.asmx");
+
+// Proxy oluştur
+var istemci = new Servis(binding, endpoint);
+
+// Çağrılar
 var sonuc = istemci.Topla(new SunucuRequest { Sayi1 = 1, Sayi2 = 2 });
-Console.WriteLine(sonuc.Sonuc);
+Console.WriteLine("Topla Sonuç: " + sonuc.Sonuc);
 
-class Servis(Binding binding, EndpointAddress remoteAddress) : ClientBase<IServis>(binding, remoteAddress)
+var cikar = istemci.Cikar(new SunucuRequest { Sayi1 = 3, Sayi2 = 1 });
+Console.WriteLine("Çıkar Sonuç: " + cikar.Sonuc);
+
+// ---- Proxy Class ----
+class Servis : ClientBase<ICalculatorService>, ICalculatorService
 {
+    public Servis(Binding binding, EndpointAddress remoteAddress)
+        : base(binding, remoteAddress) { }
+
     public SunucuResponse Topla(SunucuRequest request) => Channel.Topla(request);
+    public SunucuResponse Cikar(SunucuRequest request) => Channel.Cikar(request);
 }
 
+// ---- Service Contract (Client tarafı) ----
 [ServiceContract]
-interface IServis
-{       
-    [OperationContract]
-    SunucuResponse Topla(SunucuRequest request);
+public interface ICalculatorService
+{
+    [OperationContract] SunucuResponse Topla(SunucuRequest request);
+    [OperationContract] SunucuResponse Cikar(SunucuRequest request);
 }
 
+// ---- Data Contracts ----
 [DataContract]
 public record SunucuRequest
 {
-    [DataMember]
-    public int Sayi1 { get; set; }
-    [DataMember]
-    public int Sayi2 { get; set; }
+    [DataMember] public int Sayi1 { get; set; }
+    [DataMember] public int Sayi2 { get; set; }
 }
 
 [DataContract]
 public record SunucuResponse
 {
-    [DataMember]
-    public int Sonuc { get; set; }
+    [DataMember] public int Sonuc { get; set; }
 }
