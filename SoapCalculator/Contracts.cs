73b2acae7197bae19986e4ce4ccaf4279824561d
Harun Korkmaz <harun.korkmaz@fairteknoloji.com>
2025-09-04 17:24:56 +0300

    Refactor SOAP requests in a.http and update Contracts.cs and Dtos to support new request structure. Introduce reconciliateRequest operation and enhance LoginRequest with nested request handling.

--- SoapCalculator/Contracts.cs
+++ SoapCalculator/Contracts.cs
@@ -1,8 +1,9 @@
+using System.Runtime.Serialization;
 using System.ServiceModel;
 using SoapCalculator.Dtos;
+using SoapCalculator.PaymentGeneratedModels;
 
 namespace SoapCalculator;
-
 
 [ServiceContract(Namespace = "http://tempuri.org/")]
 public class CalculatorService
@@ -11,6 +12,12 @@ public class CalculatorService
     [OperationContract(Name = "loginRequestMethod")]
     public string LoginRequestMethod(SoapBody<LoginRequest> input)
     {
-        return $"success {input.Request.Password} {input.Request.Username}";
+        return $"success {input.Request.AnotherRequest.NestedPassword} {input.Request.AnotherRequest.NestedUsername} \n {input.Request.Password2} {input.Request.Username2}";
+    }
+
+    [OperationContract(Name = "reconciliateRequest")]
+    public string ReconciliateRequest(SoapBody<ReconciliateRequest> input)
+    {
+        return $"success {input.Request.Arg0.ReconDate} {input.Request.Arg0.ReconType} {input.Request.Arg0.BankId} {input.Request.Arg0.BankUserName}";
     }
 }
--- SoapCalculator/Dtos/general.cs
+++ SoapCalculator/Dtos/general.cs
@@ -6,26 +6,26 @@ namespace SoapCalculator.Dtos;
 [DataContract(Namespace = "http://tempuri.org/")]
 public class LoginRequest
 {
-	[DataMember(Name = "password")]
-	public string Password { get; set; } = string.Empty;
-	[DataMember(Name = "username")]
-	public string Username { get; set; } = string.Empty;
-	// [DataMember(Name = "anotherRequest")]
-	// public AnotherRequest AnotherRequest { get; set; } = new();
+	[DataMember(Name = "password2", Order = 2)]
+	public string Password2 { get; set; } = string.Empty;
+	[DataMember(Name = "username2", Order = 1)]
+	public string Username2 { get; set; } = string.Empty;
+	[DataMember(Name = "anotherRequest", Order = 3)]
+	public NestedClass AnotherRequest { get; set; } = new();
 }
 
 [DataContract(Namespace = "http://tempuri.org/")]
-public class AnotherRequest
+public class NestedClass
 {
-	[DataMember(Name = "password2")]
-	public string Password2 { get; set; } = string.Empty;
-	[DataMember(Name = "username2")]
-	public string Username2 { get; set; } = string.Empty;
+	[DataMember(Name = "password")]
+	public string NestedPassword { get; set; } = string.Empty;
+	[DataMember(Name = "username")]
+	public string NestedUsername { get; set; } = string.Empty;
 }
 
 [MessageContract(IsWrapped = false)]
 public class SoapBody<T> where T : class, new()
 {
-	[MessageBodyMember(Name = "YourRequest")]
+	[MessageBodyMember]
 	public T Request { get; set; } = new();
 }
